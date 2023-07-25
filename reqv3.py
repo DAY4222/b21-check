b4d8f3f615b72489a864ee5a1128197ca709b7d8
Justin Fuentes <118407620+DAY4222@users.noreply.github.com>
2023-07-25 16:30:14 -0700

    Merged with new req code (OUTDATED)

--- reqv3.py
+++ reqv3.py
@@ -4,34 +4,27 @@ from datetime import datetime, timedelta , time
 import time
 start_time = time.time()
 
-def preprocess_timetable(file_path):
+def find_row_number(filename, search_string):
+    row_number = None
 
-    # Read the timetable data from a text file into a DataFrame
-    df_main = pd.read_table(file_path, skiprows=14)
-    # Define a list of column indices to drop from the DataFrame
-    columns_to_drop = [1, 2, 3, 5, 6, 8, 10, 12, 13, 14, 15]
-    # Drop the specified columns from df_main
-    df_main.drop(df_main.columns[columns_to_drop], axis=1, inplace=True)
-    # Rename the columns of df_main
-    column_names = ['Train_ID', 'Station', 'Arrival Time', 'Departure Time', 'Dwell Time']
-    df_main.columns = column_names
-    return df_main
+    with open(filename, "r") as file:
+        for i, line in enumerate(file, 1):
+            if line.startswith(search_string):
+                row_number = i
+                break
+            
+    return row_number
 
-def filter_and_replace(df, search_word, replacement_dict):
-    # Filter df based on Train_ID containing the search word
-    via_filter = df["Train_ID"].str.contains(search_word)
-    via_df = df[via_filter]
-    # Replace values in via_df using the replacement_dict
-    via_df.replace(replacement_dict)
-    return via_df
+def read_timetable_data1(filename, skiprows, nrows):
+    df_timetable = pd.read_table(filename, header=None, skiprows=skiprows, nrows=nrows)
+    df_timetable.columns = ['Train_ID', "intervalCourseID", "timeToIntervalReference", "stationIndex",
+                            'Station', "trackName", "arrTimeDayOffset", 'Arrival Time', "depTimeDayOffset",
+                            'Departure Time', "useDepTime", 'Dwell Time', "stopAtStation", "meanDelay", "Distribution", "deltaMass"]
+    df_timetable = df_timetable.loc[:, ['Train_ID', 'Station', 'Arrival Time',
+                                         'Departure Time', 'Dwell Time']]
+    return df_timetable
 
-file_path = "2023-06-01 CS1 Network Timetable (OT Format).txt"
-df_main = preprocess_timetable(file_path)
-search_word = "VIA"
-replacement_dict = {"HH:MM:SS": "00:00:00", "XX:XX:XX": "00:00:00"}
-via_df = filter_and_replace(df_main, search_word, replacement_dict)
-
-def dictionary_from_data_frame(dataframe, column_name):
+def create_sub_data_frames_dict_from_dataframe2(dataframe, column_name):
     values = dataframe[column_name].unique().tolist()
     data_dict = {}
 
@@ -41,6 +34,15 @@ def dictionary_from_data_frame(dataframe, column_name):
         data_dict[value] = sub_df
 
     return data_dict
+
+filename1 = "2023-06-01 CS1 Network Timetable (OT Format).txt"
+skip_rows_needed2 = find_row_number(filename, "// Connections:")
+df_timetable = read_timetable_data1(filename1, 13, skip_rows_needed2-15)
+search_word = "VIA"
+replacement_dict = {"HH:MM:SS": "00:00:00", "XX:XX:XX": "00:00:00"}
+via_df = filter_and_replace(df_timetable, search_word, replacement_dict)
+VIA_dfs = create_sub_data_frames_dict_from_dataframe2(via_df, "Train_ID")
+
 
 def create_sub_data_frames_dict_for_input_file(excel_file_path, key_column):
     df = pd.read_excel(excel_file_path)
@@ -59,20 +61,18 @@ def create_sub_data_frames_dict_for_input_file(excel_file_path, key_column):
     
     return data_dict
 
-VIA_dfs = dictionary_from_data_frame(via_df,"Train_ID")
 VIA_Train_Input_criteria = pd.read_excel('InputExcel.xlsx')
 VIA_Train_Input_criteria_dict = create_sub_data_frames_dict_for_input_file('InputExcel.xlsx', 'Train_ID')
 
 def max_runtime_for_train(dictionary, station_start, station_end, max_run_time):
-    
+
     if len(dictionary) == 0:
         return None
-    
     for key in VIA_Train_Input_criteria_dict.keys():
         if key not in dictionary:
             print(f"Train {key} is not found in timetable given")
             continue
-    
+        
         df = dictionary[key]
         sub_dict_of_input_criteria = VIA_Train_Input_criteria_dict[key]
         end_df = df.loc[df['Station'] == station_end,"Arrival Time"]
@@ -112,6 +112,11 @@ def get_rows_with_column_value_true(dataframe, column_name, value):
     mask = dataframe[column_name] == value
     return dataframe[mask]  # Return the DataFrame slice where the mask is True
 
+def check_dwell_time_at_station(train_df, station, dwell_time_desired_sec):
+    mask = (train_df['Dwell Time'] == dwell_time_desired_sec) & (train_df['Station'] == station)
+    filtered_df = train_df[mask]
+    return not filtered_df.empty
+
 def check_if_selected_category_dwells_on_station_based_on_icon(dictionary, icon, icon_valid_value, station,dwell_time_desired_sec):
     result = []
     sorted_df = get_rows_with_column_value_true(VIA_Train_Input_criteria, icon, icon_valid_value)
@@ -129,21 +134,6 @@ def check_if_selected_category_dwells_on_station_based_on_icon(dictionary, icon,
         else:
             print(f"The key '{key}' does not exist in the dictionary.")
 
-def check_dwell_time_at_station(train_df, station, dwell_time_desired_sec):
-    mask = (train_df['Dwell Time'] == dwell_time_desired_sec) & (train_df['Station'] == station)
-    filtered_df = train_df[mask]
-    return not filtered_df.empty
-
-#TODO: FINISH ???
-def check_dwell_time_between_NRT_and_RT_at_Union():
-    trains_to_check = VIA_Train_Input_criteria_dict.keys()
-    for key in trains_to_check:
-        if key not in dictionary:
-            print(f" No matching key for {key} found in timetable")
-            return 
-        if df["Bound"] == "Inbound":
-            return
-    return
 
 #this function retrieves a specific value from a DataFrame based on a search key and the corresponding key and value columns.                   
 def get_data_by_search(df, search_key, key_column, value_column):
@@ -260,9 +250,10 @@ def find_row_number(filename, search_string):
                 break
             
     return row_number
-
 def read_connection_data(filename, skiprows):
-    df_connection_og = pd.read_table(filename, header=None, skiprows=skiprows+2)
+    columns_to_read = [0, 1, 2, 3, 4, 5, 6, 7]
+   
+    df_connection_og = pd.read_table(filename, header=None, skiprows=skiprows+2, usecols=columns_to_read)
     df_connection_og.columns = ["Train_ID", "ConnTrain_ID", "StationIndex", "ConnStation",
                                 "ConnTime", "ConnChangeTime", "ConnMaxChangeTime", "ConnectionType"]
     dfConnection = df_connection_og.drop(['ConnMaxChangeTime'], axis=1)
@@ -284,7 +275,7 @@ def create_sub_data_frames_dict_from_dataframe(dataframe, key_column):
         sub__data_frames_dict[key] = dataframe[dataframe[key_column] == key]
     return sub__data_frames_dict
 
-filename = "CS2 Network Timetable 1125.txt"
+filename = "2023-06-01 CS1 Network Timetable (OT Format).txt"
 skiprows_needed = find_row_number(filename, "// Connections:")
 dfConnection = read_connection_data(filename, skiprows_needed)
 df_timetable_that_has_connection_data = read_timetable_data(filename, 13, skiprows_needed-15)
@@ -340,41 +331,182 @@ def test_nrt_check():
     assert nrt_check(VIA_Train_Input_criteria_dict,"Star Icon", "yes","Inbound","Bound") == True
 
 
-def nrt_check_for_dwell():
+def connection_check_for_dwell(criteria_time):
     
-    for index, row in VIA_Train_Input_criteria.itertuples():
-        print(row)
+    for index,row in VIA_Train_Input_criteria.iterrows():
+        Train_ID = row["Train_ID"]
        
-    if VIA_Train_Input_criteria["Bound"].iat[0] == "Outbound":
-        connection_type = 2
-        column_nrt_found = 'Train_ID'
-        column_to_look_for_key = 'ConnTrain_ID'
-    elif VIA_Train_Input_criteria["Bound"].iat[0] == "Inbound":
-        connection_type = 2
-        column_nrt_found = "ConnTrain_ID"
-        column_to_look_for_key = 'Train_ID'
+        if row["Bound"] == "Outbound":
+            connection_type = 2
+            column_nrt_found = 'Train_ID'
+            column_to_look_for_key = 'ConnTrain_ID'
+        elif row["Bound"] == "Inbound":
+            connection_type = 2
+            column_nrt_found = "ConnTrain_ID"
+            column_to_look_for_key = 'Train_ID'
+            
+        modified_train_id = Train_ID + ".1"
+        dwell_time = dfConnection.loc[(dfConnection[column_to_look_for_key] == modified_train_id) & 
+                                    (dfConnection['ConnectionType'] == connection_type), :]
+        if dwell_time.empty:
+            print(f"No matching Connection train found for train {Train_ID}.")
+            continue
         
-    modified_train_id = VIA_Train_Input_criteria["Train_ID"].iat[0] + ".1"
-    dwell_time = dfConnection.loc[(dfConnection[column_to_look_for_key] == modified_train_id) & 
-                                  (dfConnection['ConnectionType'] == connection_type), :]
-    # print(dwell_time[connChangeTime])
-    
+        time_string = dwell_time["ConnChangeTime"].values[0]
+        time_object = datetime.strptime(time_string,"%H:%M:%S").time()
+        connection_Train_ID = dwell_time[column_nrt_found].values[0]
+        criteria = datetime.strptime(criteria_time,"%M:%S").time()
+        if 'E' not in connection_Train_ID:
+            if time_object < criteria :
+                print(f"{Train_ID} with connection {connection_Train_ID} does not meet minimum dwell time at union of {criteria} MINS")
+            continue
+        # print(Train_ID)
+        # print(dwell_time["ConnChangeTime"].values[0])
 
-nrt_check_for_dwell()
+def connection_time_check_for_NRT(criteria_time_NRT_to_Outbound,criteria_time_Inbound_to_NRT):
+        
+    for index,row in VIA_Train_Input_criteria.iterrows():
+        Train_ID = row["Train_ID"]
+       
+        if row["Bound"] == "Outbound":
+            connection_type = 2
+            column_nrt_found = 'Train_ID'
+            column_to_look_for_key = 'ConnTrain_ID'
+            criteria = criteria_time_NRT_to_Outbound
+        elif row["Bound"] == "Inbound":
+            connection_type = 2
+            column_nrt_found = "ConnTrain_ID"
+            column_to_look_for_key = 'Train_ID'
+            criteria = criteria_time_Inbound_to_NRT
+            
+        modified_train_id = Train_ID + ".1"
+        dwell_time = dfConnection.loc[(dfConnection[column_to_look_for_key] == modified_train_id) & 
+                                    (dfConnection['ConnectionType'] == connection_type), :]
+        if dwell_time.empty:
+            print(f"No matching Connection train found for train {Train_ID}.")
+            continue
+        
+        time_string = dwell_time["ConnChangeTime"].values[0]
+        time_object = datetime.strptime(time_string,"%H:%M:%S").time()
+        connection_Train_ID = dwell_time[column_nrt_found].values[0]
+        criteria = datetime.strptime(criteria,"%M:%S").time()
+        if 'E' not in connection_Train_ID:
+            continue 
+        if  time_object < criteria:
+            print(f"{Train_ID} with connection {connection_Train_ID} does not meet minimum dwell time at union of {criteria} MINS")
+    return True
+
+def test_connection_time_check_for_NRT():
+    assert connection_time_check_for_NRT("1:00","1:00")== True
+
+
+
+# connection_check_for_dwell("20:00")
+# connection_time_check_for_NRT("10:00","10:00")
 #nrt_check_for_dwell(VIA_Train_Input_criteria_dict,"select_all","yes","Outbound")
+#station_stop_check(VIA_dfs,"Guildwood")
+#check_if_selected_category_dwells_on_station_based_on_icon(VIA_dfs, "Cross Icon", "yes", "Guildwood Station",60)
 
-
-
-
-
-
-
-
-#TODO: DWELL TIME CHECK E TO NON E TABLE 9 first step done part 2 next dict created, 
-# now match find time and sum time.
 #TODO:655,VIA60 exception need to be coded in SAME WITH THE / TRAINS, potential only issue for old datafile,
 #investigate when new timetable is here.
 #TODO:2b iv)exception 88 which may bypass malton station, will be fixed in bus logic output writing
+
+
+
+def test_box1():
+    test_max_runtime_for_train()
+    test_nrt_check()
+    test_station_stop_check()
+    test_check_last_value_in_range()
+test_box1()
+print("Process finished --- %s seconds ---" % (time.time() - start_time))
+
+def max_runtime_for_trainv_for_1(dictionary, station_start, station_end, max_run_time):
+
+    if len(dictionary) == 0:
+        return None
+    
+    for key in VIA_Train_Input_criteria_dict.keys():
+        key = key + ".1"
+        
+        if key not in dictionary:
+            print(f"Train {key} is not found in timetable given")
+            continue
+            
+        df = dictionary[key]
+        key = key[:-2]
+        sub_dict_of_input_criteria = VIA_Train_Input_criteria_dict[key]
+        end_df = df.loc[df['Station'] == station_end,"Arrival Time"]
+        start_df = df.loc[df['Station'] == station_start,"Departure Time"]
+        
+        if len(end_df.index) == 0 or end_df.values[0] == "HH:MM:SS":
+            end_df = df.loc[df['Station'] == station_end,"Departure Time"]
+        
+        if len(start_df.index) == 0 or  start_df.values[0] == "HH:MM:SS":
+            start_df = df.loc[df['Station'] == station_start,"Arrival Time"]   
+        
+        if start_df.empty or end_df.empty:
+            #print(f"{key} does not stop at one of the stations :(")
+            continue
+       
+        Departure_Time = start_df.values[0]
+        Arrival_Time = end_df.values[0]
+    
+        start_time = datetime.strptime(Departure_Time, "%H:%M:%S")
+        end_time = datetime.strptime(Arrival_Time, "%H:%M:%S")
+
+        time_diff = abs(end_time - start_time)
+        max_run_time_converted = datetime.strptime(max_run_time,"%H:%M:%S") - datetime.strptime("00:00:00", "%H:%M:%S")
+        
+        if time_diff <= max_run_time_converted:
+            print(f"The Train {key} runtime for the stations selected is below the the max, {time_diff}")
+        else:
+            print(f"{key} failed Max Run Time of {max_run_time}, actual runtime: {time_diff}")
+    return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #TODO:FIX MIDNIGHT EDGE CASE I don't think this is an issue, module takes care of it but worth a test!VIA 78
 #POSSIBLE FIX TO MIDNIGHT RANGE ISSUE....
 #time_format = "%H:%M:%S"  # 24-hour format with seconds
@@ -391,16 +523,3 @@ nrt_check_for_dwell()
 #    print("23:45 PM is between 23:30 PM and 1 AM")
 #else:
 #    print("23:45 PM is not between 23:30 PM and 1 AM")
-
-#station_stop_check(VIA_dfs,"Guildwood")
-#check_if_selected_category_dwells_on_station_based_on_icon(VIA_dfs, "Cross Icon", "yes", "Guildwood Station",60)
-def test_box1():
-    test_max_runtime_for_train()
-    test_nrt_check()
-    test_station_stop_check()
-    test_check_last_value_in_range()
-
-#test_box1()
-print("Process finished --- %s seconds ---" % (time.time() - start_time))
-
-
